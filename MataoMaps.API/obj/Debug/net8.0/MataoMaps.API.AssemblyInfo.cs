f512a5da679e398d70b3a1b155a07398dd3f19c5
Quévin <thexof02@gmial.com>
2024-09-27 12:31:24 -0300

    Atualizado NavMenu

--- MataoMaps.API/obj/Debug/net8.0/MataoMaps.API.AssemblyInfo.cs
+++ MataoMaps.API/obj/Debug/net8.0/MataoMaps.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MataoMaps.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88cb60eb7e6429a449267b2c2bf8d70ec26b4c15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e2b352dd07cd5cd919df7f2e09d7b2b090e2575")]
 [assembly: System.Reflection.AssemblyProductAttribute("MataoMaps.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MataoMaps.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
