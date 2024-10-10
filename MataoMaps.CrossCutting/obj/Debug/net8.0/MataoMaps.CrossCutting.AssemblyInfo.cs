96d52979e28f18e233ccb7d69ab328eeb4474c79
WilliamSilvaGh <williamsilvaesquilino@gmail.com>
2024-10-10 10:52:14 -0300

    Adicionado a área administrativa e a listagem de usuários por tabela

--- MataoMaps.CrossCutting/obj/Debug/net8.0/MataoMaps.CrossCutting.AssemblyInfo.cs
+++ MataoMaps.CrossCutting/obj/Debug/net8.0/MataoMaps.CrossCutting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MataoMaps.CrossCutting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cbd15ddf8267a249007476799e225b371552c23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f80a796f2aaf90078ea7ff7d3e3fbffd8f17d9a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MataoMaps.CrossCutting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MataoMaps.CrossCutting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MataoMaps.Presentation/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MataoMaps.Presentation.AssemblyInfo.cs
+++ MataoMaps.Presentation/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MataoMaps.Presentation.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MataoMaps.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0cbd15ddf8267a249007476799e225b371552c23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f80a796f2aaf90078ea7ff7d3e3fbffd8f17d9a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MataoMaps.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MataoMaps.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
