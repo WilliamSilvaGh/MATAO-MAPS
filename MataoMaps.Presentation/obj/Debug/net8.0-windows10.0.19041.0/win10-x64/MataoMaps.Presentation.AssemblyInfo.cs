bc2bd0de72588c1589e0ae6b4eddc74d717b021b
William Esquilino <williamsilvaesquilino@gmail.com>
2024-10-11 04:53:42 -0300

    Adicionado a paginação, trocado os ícones do side bar e outros ajustes

--- MataoMaps.Presentation/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MataoMaps.Presentation.AssemblyInfo.cs
+++ MataoMaps.Presentation/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MataoMaps.Presentation.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MataoMaps.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+96d52979e28f18e233ccb7d69ab328eeb4474c79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fe46b8eb119685bc73b7e1ee18398c1782308641")]
 [assembly: System.Reflection.AssemblyProductAttribute("MataoMaps.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MataoMaps.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
