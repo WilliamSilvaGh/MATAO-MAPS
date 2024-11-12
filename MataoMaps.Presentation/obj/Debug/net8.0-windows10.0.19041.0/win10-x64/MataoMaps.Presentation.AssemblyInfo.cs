31fc0b816805feabcd150022d2c33b201666a839
William Esquilino <williamsilvaesquilino@gmail.com>
2024-11-12 14:06:40 -0300

    Adicionado o tratamento de erros com notificações para o usuário

--- MataoMaps.Presentation/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MataoMaps.Presentation.AssemblyInfo.cs
+++ MataoMaps.Presentation/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MataoMaps.Presentation.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MataoMaps.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+433e7ff53bcb763464252bd8c0e09462bfc243d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6b28ab391145ec221d8eff3b9f751181f3bbb60d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MataoMaps.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MataoMaps.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
