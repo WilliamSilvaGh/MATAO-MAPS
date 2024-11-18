9998245d8b21f1caf4e1d845142fd607cecfef03
William Esquilino <williamsilvaesquilino@gmail.com>
2024-11-18 06:16:11 -0300

    Criado o ajuste automático da tela para quando o teclado é aberto no celular

--- MataoMaps.Presentation/Platforms/Android/MainActivity.cs
+++ MataoMaps.Presentation/Platforms/Android/MainActivity.cs
@@ -1,11 +1,19 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace MataoMaps.Presentation
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Configurar o modo de ajuste do teclado
+            Window.SetSoftInputMode(SoftInput.AdjustResize);
+        }
     }
 }
